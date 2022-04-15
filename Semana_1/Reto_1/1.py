f8a64cb8f35bb71b4696e950171f95c5df51af0c
Sebastian Cavero <sebas13.scl@gmail.com>
2022-04-15 17:13:41 -0500

    Reto_1-terminado
    Solución del reto 1 terminada

---
+++ Semana_1/Reto_1/1.py
@@ -0,0 +1,10 @@
+import string
+
+
+nombre = input("Ingrese su nombre: ")
+edad = int(input("Ingrese su edad: "))
+
+if edad < 18:
+    print("Si son más de las 6pm por favor debe ir a dormir")
+else:
+    print("Tiene carta libre a hacer lo que le da la gana")
